a3ab4b21ff77d6494db36bcd1d7b2efa65adc6d5
Adrian Leblanc Morales <adrian.leblanc@generacc.cl>
2023-07-25 09:51:13 -0400

    Cambio de nombre en controlador

--- src/main/java/com/generacc/backend/calidad/backendcalidad/controllers/general/GeneralController.java
+++ src/main/java/com/generacc/backend/calidad/backendcalidad/controllers/general/GeneralController.java
@@ -1,9 +1,7 @@
 package com.generacc.backend.calidad.backendcalidad.controllers.general;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
